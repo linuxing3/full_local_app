844ba082065427564d96bc775e37f15ff242d048
linuxing3 <linuxing3@qq.com>
2024-03-11 00:09:41 -0400

    [fix] articles test

--- tests/requests/articles.rs
+++ tests/requests/articles.rs
@@ -4,26 +4,21 @@ use serial_test::serial;
 
 #[tokio::test]
 #[serial]
-async fn can_get_echo() {
-    testing::request::<App, _, _>(|request, _ctx| async move {
-        let payload = serde_json::json!({
-            "foo": "bar",
-        });
-
-        let res = request.post("/articles/echo").json(&payload).await;
-        assert_eq!(res.status_code(), 200);
-        assert_eq!(res.text(), serde_json::to_string(&payload).unwrap());
-    })
-    .await;
-}
-
-#[tokio::test]
-#[serial]
-async fn can_request_root() {
+async fn can_request_find_all() {
     testing::request::<App, _, _>(|request, _ctx| async move {
         let res = request.get("/articles").await;
         assert_eq!(res.status_code(), 200);
-        assert_eq!(res.text(), "hello");
+        assert_eq!(res.text(), "[{\"created_at\":\"2024-03-11T00:58:53.973576\",\"updated_at\":\"2024-03-11T00:58:53.973576\",\"id\":1,\"title\":\"how to build apps in 3 steps\",\"content\":\"use Loco: https://loco.rs\"},{\"created_at\":\"2024-03-11T01:20:24.843516\",\"updated_at\":\"2024-03-11T01:20:24.843516\",\"id\":2,\"title\":\"how to build apps in 3 steps\",\"content\":\"use Loco: https://loco.rs\"}]");
+    })
+    .await;
+}
+#[tokio::test]
+#[serial]
+async fn can_request_find_first() {
+    testing::request::<App, _, _>(|request, _ctx| async move {
+        let res = request.get("/articles/1").await;
+        assert_eq!(res.status_code(), 200);
+        assert_eq!(res.text(), "{\"created_at\":\"2024-03-11T00:58:53.973576\",\"updated_at\":\"2024-03-11T00:58:53.973576\",\"id\":1,\"title\":\"how to build apps in 3 steps\",\"content\":\"use Loco: https://loco.rs\"}");
     })
     .await;
 }
