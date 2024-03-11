1e0b503988ec5bee8792ff7201037bda8d1dff30
linuxing3 <linuxing3@qq.com>
2024-03-11 00:16:58 -0400

    [fix] articles test without init

--- tests/requests/articles.rs
+++ tests/requests/articles.rs
@@ -8,7 +8,7 @@ async fn can_request_find_all() {
     testing::request::<App, _, _>(|request, _ctx| async move {
         let res = request.get("/articles").await;
         assert_eq!(res.status_code(), 200);
-        assert_eq!(res.text(), "[{\"created_at\":\"2024-03-11T00:58:53.973576\",\"updated_at\":\"2024-03-11T00:58:53.973576\",\"id\":1,\"title\":\"how to build apps in 3 steps\",\"content\":\"use Loco: https://loco.rs\"},{\"created_at\":\"2024-03-11T01:20:24.843516\",\"updated_at\":\"2024-03-11T01:20:24.843516\",\"id\":2,\"title\":\"how to build apps in 3 steps\",\"content\":\"use Loco: https://loco.rs\"}]");
+        assert_eq!(res.text(), "[]");
     })
     .await;
 }
@@ -18,7 +18,7 @@ async fn can_request_find_first() {
     testing::request::<App, _, _>(|request, _ctx| async move {
         let res = request.get("/articles/1").await;
         assert_eq!(res.status_code(), 200);
-        assert_eq!(res.text(), "{\"created_at\":\"2024-03-11T00:58:53.973576\",\"updated_at\":\"2024-03-11T00:58:53.973576\",\"id\":1,\"title\":\"how to build apps in 3 steps\",\"content\":\"use Loco: https://loco.rs\"}");
+        assert_eq!(res.text(), "{}");
     })
     .await;
 }
